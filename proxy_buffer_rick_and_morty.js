2f56d5ba74eef19eb87fc2fb8b2589e9ca7b2950
Rajchal <rajchalanjal1@gmail.com>
2024-12-31 23:27:33 +0545

    test first commit

---
+++ proxy_buffer_rick_and_morty.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+async function fetchChar(id){
+   try{
+    const character=await axios.get(
+        `https://rickandmortyapi.com/api/chracter/${id}`;
+    )
+   }
+}
+const characterCache ={};
+
+const cacheHandler={
+    get: async(id){
+        if(target(id)){
+            return target[id];
+        }
+
+        const character=await fetchChar(id);
+
+        characterCacher[id]={...character,time: new Date()}
+
+        return character;
+    }
+};
+
+const getChar = new Proxy (charachterCache , cacheHandler)
